ba954fc5324d2f92e6df0a98903ef811b0f8a854
Sarcee23 <146372411+Sarcee23@users.noreply.github.com>
2026-08-15 03:04:54 +0530

    Solve covariance-matrix

---
+++ covariance-matrix/covariance-matrix.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def covariance_matrix(X):
+    """
+    Compute covariance matrix from dataset X.
+    """
+    X = np.asarray(X)
+
+   
+    if X.ndim != 2 or X.shape[0] < 2:
+        return None
+
+
+    mean = np.mean(X, axis=0)
+    X_centered = X - mean
+
+
+    cov = (X_centered.T @ X_centered) / (X.shape[0] - 1)
+
+    return cov
